b2a29775dba7110ac38b0a6e825282463295fd47
utop1an <vfqq1066@outlook.com>
2023-02-02 22:33:29 +1100

    fix: main.ts listen on env.port

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(`${process.env.port}`);
 }
 bootstrap();
