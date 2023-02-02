1efc42660abc290af507930d17ee4ec6fdf86686
utop1an <vfqq1066@outlook.com>
2023-02-02 22:12:30 +1100

    fix: update pm2 eco conf

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,15 @@
 module.exports = {
   apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
+    name: 'todo-server',
+    script: 'dist/main.js',
+    env: {
+      STAGE: "dev"
+    },
+    env_prod: {
+      STAGE: "prod"
+    }
+
   }],
 
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+  
 };
