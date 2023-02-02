fd9a1e5768f31821f98ba92c15944b6a0f009782
utop1an <vfqq1066@outlook.com>
2023-02-02 22:13:42 +1100

    fix: update pm2 eco conf

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,10 +3,13 @@ module.exports = {
     name: 'todo-server',
     script: 'dist/main.js',
     env: {
+      port: 3000,
       STAGE: "dev"
     },
     env_prod: {
+      port: 80,
       STAGE: "prod"
+
     }
 
   }],
